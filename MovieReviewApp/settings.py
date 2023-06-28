d09dc80d7364ebc6532631b7e4ee021cee032e58
Mauricio Lopez <lopezsmauricio@gmail.com>
2023-06-28 15:56:11 -0500

    Created core and movies apps, installed debug toolbar and rest framework packages, created custom user model in core app

--- MovieReviewApp/settings.py
+++ MovieReviewApp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'rest_framework',
+    'movies', 
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +51,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'MovieReviewApp.urls'
